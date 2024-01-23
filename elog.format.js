9ed3a7526a56464ad91cc3d4c63a529f10444287
Cnzw <git@cnzw.top>
2024-01-23 19:44:53 +0800

    Elog 初始化

--- elog.format.js
+++ elog.format.js
@@ -1,16 +1,20 @@
-const {matterMarkdownAdapter} = require('@elog/plugin-adapter')
+// 0.12.0及以上版本用法
+const { matterMarkdownAdapter } = require('@elog/cli')
 
-// 自定义文档处理器
-// doc的类型定义为 DocDetail，详情见下方 DocDetail 类型定义
-const format = (doc) => {
-  // ...对文档进行处理
-  const theDate = doc.properties.date.string
-  const theUpdate = doc.properties.updated.string
+/**
+ * 自定义文档插件
+ * @param {DocDetail} doc doc的类型定义为 DocDetail
+ * @param {ImageClient} imageClient 图床下载器，可用于图片上传
+ * @return {Promise<DocDetail>} 返回处理后的文档对象
+ */
+const format = (doc, imageClient) => {
+  const theDate = doc.properties.date
+  const theUpdate = doc.properties.updated
   doc.properties.urlname = theDate.split(' ')[0] +'-'+ doc.properties.title
   doc.properties.date = theDate
   doc.properties.updated = theUpdate
-  const newPost = matterMarkdownAdapter(doc);
-  return newPost;
+  doc.body = matterMarkdownAdapter(doc);
+  return doc;
 };
 
 module.exports = {
